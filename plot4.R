aa5836964fc87e504c7ae5a519ffc7a8550ce292
simon19891101 <simon19891101@gmail.com>
2016-09-23 23:33:31 +1200

    Add files via upload

---
+++ plot4.R
@@ -0,0 +1,13 @@
+Origin_data <- read.table("./household_power_consumption.txt",header = TRUE,sep = ";",na.strings='?')
+Target_data <- subset(Origin_data, as.Date(Date,"%d/%m/%Y") >= "2007-02-01" & as.Date(Date,"%d/%m/%Y") <= "2007-02-02")
+
+png(filename = "plot4.png",width = 480, height = 480, units = "px")
+par(mfrow=c(2,2),mar=c(5,4,5,2))
+with(Target_data,plot(strptime(paste(Date,Time),"%d/%m/%Y %H:%M:%S"),Global_active_power,type = "l",ylab = "Global Active Power",xlab = ""))
+with(Target_data,plot(strptime(paste(Date,Time),"%d/%m/%Y %H:%M:%S"),Voltage,type = "l",ylab = "Voltage",xlab = "datetime"))
+with(Target_data,plot(strptime(paste(Date,Time),"%d/%m/%Y %H:%M:%S"),Sub_metering_1,type = "l",ylab = "Energy sub metering",xlab = ""))
+with(Target_data,lines(strptime(paste(Date,Time),"%d/%m/%Y %H:%M:%S"),Sub_metering_2,type = "l",col="red"))
+with(Target_data,lines(strptime(paste(Date,Time),"%d/%m/%Y %H:%M:%S"),Sub_metering_3,type = "l",col="blue"))
+legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"),lty = c(1,1,1))
+with(Target_data,plot(strptime(paste(Date,Time),"%d/%m/%Y %H:%M:%S"),Global_reactive_power,type = "l",ylab = "Global_reactive_power",xlab = "datetime"))
+dev.off()
